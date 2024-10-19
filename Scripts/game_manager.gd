262d3b38260f4b35414fe6cd19bf7053894e4d14
Rosling <srosemiller.26@gmail.com>
2024-10-19 19:18:19 -0400

    win screen

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,6 +1,9 @@
 extends Node
+@onready var bunny = %bunny
+@onready var cat = %cat
+@onready var win = $"../CanvasLayer2/win"
 
-var score = 0
+var score = 2
 
 #@onready var score_label = $"score label"
 
@@ -8,3 +11,10 @@ func add_point():
 	score += 1
 	#score_label.text = "You collected " + str(score) + " coins."
 	print(score)
+
+func _on_win_area_body_entered(body: PhysicsBody2D):
+	if (cat in body and bunny in body) and score == 2:
+		win.visible
+		get_tree().reload_current_scene()
+		
+		
