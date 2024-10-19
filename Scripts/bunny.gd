dc6303412f981439bc39485c7d27b6631ef5ff74
Rosling <srosemiller.26@gmail.com>
2024-10-19 01:16:07 -0400

    camera adventures

--- Scripts/bunny.gd
+++ Scripts/bunny.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -8,11 +8,15 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	global_position = (player1.global_position + player2.global_position) * 0.5
-	var zoomFac1 = abs(player1.global_position.x - player2.global_position.x) * 0.5
-	var zoomFac2 = abs(player1.global_position.y - player2.global_position.y) * 0.5
+	
+	var zoomFac1 = abs(player1.global_position.x - player2.global_position.x)/5
+	var zoomFac2 = abs(player1.global_position.y - player2.global_position.y)/5
 	var zoomFac = max(max(zoomFac1, zoomFac2), 0.5)
-	print(zoomFac, zoomFac1, zoomFac2)
+	
+	print(zoomFac, " ", zoomFac1, " ", zoomFac2)
 	
 	zoom = Vector2(zoomFac, zoomFac)
+	
+
