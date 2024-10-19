bf5824addd962b824883c72e256a15d13c1ffa69
riana-schultz <162646365+riana-schultz@users.noreply.github.com>
2024-10-19 15:28:09 -0400

    pick up fish

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var score = 0
+
+#@onready var score_label = $"score label"
+
+func add_point():
+	score += 1
+	#score_label.text = "You collected " + str(score) + " coins."
+	print(score)
