bf9f72fa16032ef74549952b28197b5ce0720526
riana-schultz <162646365+riana-schultz@users.noreply.github.com>
2024-10-19 12:39:47 -0400

    kill zone for 1 or 2 dead

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -5,6 +5,7 @@ extends Area2D
 @onready var cat = $"../Players/cat"
 var player = cat
 var otherPlayer = bunny
+var count = 0
 
 func _on_body_entered(body):
 	if body == cat:
@@ -13,16 +14,20 @@ func _on_body_entered(body):
 	else:
 		player = bunny
 		otherPlayer = cat
-	
+	count += 1
 	print("You died!")
 	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").disabled = true
+	#body.get_node("CollisionShape2D").set_disabled.call_deferred(true)
 	timer.start()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
-	player.get_node("CollisionShape2D").disabled = false
+	
+	#player.get_node("CollisionShape2D").disabled = false
 	player.velocity = Vector2(0,0)
+	if count == 2:
+		get_tree().reload_current_scene()
 	player.position = otherPlayer.position
+	count = 0
 	
 	#get_tree().reload_current_scene()
