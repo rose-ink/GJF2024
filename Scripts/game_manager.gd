24e3ed47245f20929f755c91e19de704ae94b67a
Rosling <srosemiller.26@gmail.com>
2024-10-19 19:30:03 -0400

    fixed position and win screen

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,6 +2,7 @@ extends Node
 @onready var bunny = %bunny
 @onready var cat = %cat
 @onready var win = $"../CanvasLayer2/win"
+@onready var win_area = $"../win area"
 
 var score = 2
 
@@ -13,8 +14,10 @@ func add_point():
 	print(score)
 
 func _on_win_area_body_entered(body: PhysicsBody2D):
-	if (cat in body and bunny in body) and score == 2:
-		win.visible
+	print(body.name)
+	if (cat in win_area.get_overlapping_bodies() and bunny in win_area.get_overlapping_bodies()) and score == 2:
+		win.visible = true
+		await get_tree().create_timer(5.0).timeout
 		get_tree().reload_current_scene()
 		
 		
