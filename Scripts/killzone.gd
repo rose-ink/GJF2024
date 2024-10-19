ec6e10376e36dc2c1781555d0c7bfb6648d66b7c
riana-schultz <162646365+riana-schultz@users.noreply.github.com>
2024-10-19 12:12:21 -0400

    kill zone update

--- Scripts/killzone.gd
+++ Scripts/killzone.gd
@@ -1,13 +1,28 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var bunny = $"../Players/bunny"
+@onready var cat = $"../Players/cat"
+var player = cat
+var otherPlayer = bunny
 
 func _on_body_entered(body):
+	if body == cat:
+		player = cat
+		otherPlayer = bunny
+	else:
+		player = bunny
+		otherPlayer = cat
+	
 	print("You died!")
 	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
+	body.get_node("CollisionShape2D").disabled = true
 	timer.start()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
-	get_tree().reload_current_scene()
+	player.get_node("CollisionShape2D").disabled = false
+	player.velocity = Vector2(0,0)
+	player.position = otherPlayer.position
+	
+	#get_tree().reload_current_scene()
