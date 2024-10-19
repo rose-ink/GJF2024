0e28d8637038ea9eb5fd9585b91325bd54e0ec63
riana-schultz <162646365+riana-schultz@users.noreply.github.com>
2024-10-19 19:28:58 -0400

    death scene pt 2

--- Scripts/Timer.gd
+++ Scripts/Timer.gd
@@ -17,5 +17,6 @@ func _process(_delta):
 	label.text = "%02d:%02d" % time_left_to_live()
 
 func _on_timer_timeout():
-	#lose.visible
+	lose.visible = true
+	await get_tree().create_timer(5.0).timeout
 	get_tree().reload_current_scene()
