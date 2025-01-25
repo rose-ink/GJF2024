1ede9c5184d9e2d05f79ca702501e632bdf7d748
Rosling <srosemiller.26@gmail.com>
2025-01-25 16:37:39 -0500

    fixed one collision thing

--- Scripts/teleport2.gd
+++ Scripts/teleport2.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
 func _on_body_entered(_body: PhysicsBody2D):
-	get_tree().change_scene_to_file("res://Scenes/grass_area.tscn")
+	get_tree().change_scene_to_file.bind("res://Scenes/grass_area.tscn").call_deferred()
