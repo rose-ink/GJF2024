b32c0a7d8dd07c5f13fb4d1dbf2e0dfe0530875a
Rosling <srosemiller.26@gmail.com>
2025-01-25 21:14:29 -0500

    stuff

---
+++ Scripts/pause_menu.gd
@@ -0,0 +1,22 @@
+extends Control
+@onready var pause_menu = $"."
+
+func resume():
+	print("at resume")
+	pause_menu.visible = false
+	get_tree().paused = false
+
+func pause():
+	print("at pause")
+	pause_menu.visible = true
+	get_tree().paused = true
+	
+func _on_resume_pressed():
+	resume()
+
+func _on_quit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		pause()
