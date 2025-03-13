f0b9e6c33855c9eba04d9e0a95e038fb51a33e60
Rosling <srosemiller.26@gmail.com>
2025-03-13 11:48:26 -0400

    changed instruction text to reflect taylorcade controls

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -16,7 +16,7 @@ func add_point():
 	score += 1
 	print(score)
 
-func _on_win_area_body_entered(body: PhysicsBody2D):
+func _on_win_area_body_entered(_body: PhysicsBody2D):
 	if (cat in win_area.get_overlapping_bodies() and bunny in win_area.get_overlapping_bodies()) and score >= 2:
 		await get_tree().create_timer(1.0).timeout
 		get_tree().change_scene_to_file("res://Scenes/win_menu.tscn")
